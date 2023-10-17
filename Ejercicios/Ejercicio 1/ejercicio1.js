a527280386c2e5ecef7619c06dbfa625b78b04a2
Moon Woman <114995066+moonwomancutsthecircle@users.noreply.github.com>
2023-10-17 09:16:51 +0200

    Update ejercicio1.js
    Just some comments to understand the code.

--- Ejercicios/Ejercicio 1/ejercicio1.js
+++ Ejercicios/Ejercicio 1/ejercicio1.js
@@ -1,13 +1,16 @@
-// window.onload = calcular;
-
 
 function calcular(){
+
+    //leo primero los valores de las variables introducidas
     
     let propina = parseFloat(document.getElementById("propina").value);
     let total = parseFloat(document.getElementById("total").value);
 
+    //creo la variable que almacenará la operación
 
     let resultado = (total*propina)/100;
+
+    /*para introducir el valor de resultado en el cuadro de texto que he creado, lo leo primero, y para que no me aparezca NaN antes de que se haga la operación pongo un condicional que me compruebe si NaN, cuando NaN me de falso, entonces me escribirá el resultado, mientras de verdadero no habrá nada en el cuadro de texto*/
 
     let calculo = document.getElementById("calculo");
 
